1bf05ac403858710b28a44caf9feddbc4507c1d2
Matthew-Burfield <m1.burfield@gmail.com>
2017-01-13 19:59:00 +1300

    added sadditional api call for pokemon specifics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,86 @@
 import {render} from 'react-dom';
 import React, {Component} from 'react';
 import fetch from 'isomorphic-fetch';
+import style from '../styles/main.scss';
+
+
+class PokemonInfo extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      data: [],
+      fetched: false,
+      loading: false
+    }
+  }
+
+  componentWillMount () {
+    this.setState ({
+      loading: true
+    });
+
+    fetch(this.props.url).then(res=>res.json())
+      .then(response=>{
+        this.setState({
+          data : response,
+          loading : false,
+          fetched : true
+        });
+      });
+  }
+  render () {
+    const {data, loading, fetched} = this.state;
+      // display the pokemon data
+      if (fetched) {
+        return <div className="pokemon--species--name">
+          <div> Name: {data.name} </div>
+          <div> Weight: {data.weight} </div>
+          <div> Height: {data.height} </div>
+        </div>;
+      } else {
+        return <div className="pokemon--species--name"> POKEMON INFO LOADING... </div>
+      }
+  }
+}
 
 
 //The Pokemon component will show an individual Pokemon monster
 // It shows an image of the Pokemon and
 // shows the name of it as well.
-class Pokemon extends Component{
-  render(){
+class Pokemon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classList: "pokemon--species",
+      pokemonSelected: false
+    }
+    this.handleSelectedPokemon = this.handleSelectedPokemon.bind(this);
+  }
+  handleSelectedPokemon(e) {
+    if (this.state.classList === "pokemon--species") {
+      this.setState({
+        classList: "pokemon--species pokemon--species--selected",
+        pokemonSelected: true
+      });
+    } else {
+      this.setState({
+        classList: "pokemon--species",
+        pokemonSelected: false
+      })
+    }
+  }
+  render() {
     const {pokemon,id} = this.props;
     const fillLeadingZeros = (number)=> {
       return (number < 10) ? ("00" + number) : (number < 100) ? ("0" + number) : number;
     }
-    return <div className="pokemon--species">
+    return <div className={this.state.classList} onClick={this.handleSelectedPokemon}>
             <div className="pokemon--species--container">
               <div className="pokemon--species--sprite">
                 <img src={`/public/sprites/${id}.png`} />
               </div>
-              <div className="pokemon--species--name"> #{fillLeadingZeros(id)} {pokemon.name} </div>
+              {!this.state.pokemonSelected && <div className="pokemon--species--name"> #{fillLeadingZeros(id)} {pokemon.name} </div>}
+              {this.state.pokemonSelected && <PokemonInfo id={this.props.id} url={this.props.pokemon.url} />}
             </div>
           </div>;
     }
@@ -30,7 +93,7 @@ class Pokemon extends Component{
 //then displays them using the Pokemon Component
 
 class PokemonList extends Component{
-  constructor(props){
+  constructor (props) {
     super(props);
     this.state = {
       species : [],
@@ -38,32 +101,35 @@ class PokemonList extends Component{
       loading : false,
     };
   }
+
   componentWillMount () {
-    this.setState({
-      loading : true
-    });
-    fetch('http://pokeapi.co/api/v2/pokemon?limit=151').then(res=>res.json())
-    .then(response=>{
+    if (!this.state.fetched) {
       this.setState({
-        species : response.results,
-        loading : true,
-        fetched : true
+        loading : true
       });
-    });
+      fetch('http://pokeapi.co/api/v2/pokemon?limit=151').then(res=>res.json())
+      .then(response=>{
+        this.setState({
+          species : response.results,
+          loading : false,
+          fetched : true
+        });
+      });
+    }
   }
 
-  render(){
+  render () {
     const {fetched, loading, species} = this.state;
     let content ;
-    if(fetched){
+    if (fetched) {
       content = <div className="pokemon--species--list">{species.map((pokemon,index)=><Pokemon key={pokemon.name} id={index+1} pokemon={pokemon}/>)}</div>;
-    }else if(loading && !fetched){
-        content = <p> Loading ...</p>;
+    } else if (loading && !fetched) {
+      content = <p> Loading ...</p>;
     }
-    else{
+    else {
       content = <div/>;
     }
-    return  <div>
+    return <div>
       {content}
     </div>;
   }
@@ -72,12 +138,14 @@ class PokemonList extends Component{
 
 //This is the root component
 class PokeApp extends Component{
-  render(){
+  render () {
     return <div className="pokeapp">
       <h1> The Kanto PokeDex! </h1>
-      <PokemonList/>
+      <PokemonList />
     </div>;
   }
 }
 
-render(<PokeApp/>,document.getElementById('app'))
+render(<PokeApp />,document.getElementById('app'))
+
+
